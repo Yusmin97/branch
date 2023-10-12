ff26ec98feeec1e8a5d21f0f3d485c9b996da561
Yusmin97 <tmdamsi97@gmail.com>
2023-10-12 11:33:17 +0900

    충돌연습

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-Type': 'text/planin' });
-  res.write('향마루');
+  res.write('소사');
   res.end();
 });
 
